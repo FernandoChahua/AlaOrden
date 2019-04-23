9164cea818b60190a14a4c1fab8db038cdbc7857
JALG99 <jorgelandaverry@hotmail.com>
2019-04-23 14:32:10 -0500

    Creacion de Categoria Repository

--- core/src/main/java/com/myorg/core/service/ICategoriaRepository.java
+++ core/src/main/java/com/myorg/core/service/ICategoriaRepository.java
@@ -2,6 +2,6 @@ package com.myorg.core.service;
 
 import com.myorg.core.entity.Categoria;
 
-public class ICategoriaRepository {
+public interface ICategoriaRepository extends ICrudService<Categoria> {
 
 }
