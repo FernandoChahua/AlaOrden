d84721644c4948214aeda5b1704e792068d83258
marco <jitrix15@hotmail.com>
2019-04-23 15:16:20 -0500

    Agregado archivo para los visores

--- web/src/main/java/com/myorg/web/controller/FranquiciaController.java
+++ web/src/main/java/com/myorg/web/controller/FranquiciaController.java
@@ -13,109 +13,109 @@ import org.primefaces.event.SelectEvent;
 
 @Named
 @ViewScoped
-public class FranquiciaController implements Serializable{
+public class FranquiciaController implements Serializable {
+
     private static final long serialVersionUID = 1L;
-    
+
     @Inject
     private IFranquiciaService franquiciaService;
-    
+
     private Franquicia franquicia;
     private Franquicia franquiciaSel;
     private List<Franquicia> franquicias;
 
     @PostConstruct
-    public void init(){
+    public void init() {
         franquicia = new Franquicia();
         franquiciaSel = new Franquicia();
         loadFranquicias();
     }
-    
-    public void loadFranquicias(){
+
+    public void loadFranquicias() {
         try {
             this.franquicias = franquiciaService.findAll();
-	} catch (Exception e) {
-            
-	}
-    } 
-   
+        } catch (Exception e) {
+
+        }
+    }
+
     public void saveFranquicia() {
-		try {
-			if (franquicia.getIdFranquicia()!= 0) {
-				franquiciaService.update(franquicia);
-				Message.messageInfo("Registro actualizado exitosamente");
-			} else {
-				franquiciaService.insert(franquicia);
-				Message.messageInfo("Registro guardado exitosamente");
+        try {
+            if (franquicia.getIdFranquicia() != 0) {
+                franquiciaService.update(franquicia);
+                Message.messageInfo("Registro actualizado exitosamente");
+            } else {
+                franquiciaService.insert(franquicia);
+                Message.messageInfo("Registro guardado exitosamente");
 
-			}
-			loadFranquicias();
-			cleanForm();
-		} catch (Exception e) {
-			Message.messageError("Error FranquiciaType :" + e.getMessage());
-		}
-	}
+            }
+            loadFranquicias();
+            cleanForm();
+        } catch (Exception e) {
+            Message.messageError("Error FranquiciaType :" + e.getMessage());
+        }
+    }
 
-	public void editFranquicia() {
-		try {
-			if (this.franquiciaSel.getIdFranquicia() > 0) {
-				this.franquicia = this.franquiciaSel;
-				
-			} else {
-				Message.messageInfo("Debe seleccionar un  franquiciao");
-			}
-		} catch (Exception e) {
-			Message.messageError("Error Franquicia :" + e.getMessage());
-		}
+    public void editFranquicia() {
+        try {
+            if (this.franquiciaSel.getIdFranquicia() > 0) {
+                this.franquicia = this.franquiciaSel;
 
-	}
+            } else {
+                Message.messageInfo("Debe seleccionar un  franquiciao");
+            }
+        } catch (Exception e) {
+            Message.messageError("Error Franquicia :" + e.getMessage());
+        }
 
-	public void deleteFranquicia() {
-		try {
-			if (this.franquiciaSel != null) {
-				franquiciaService.delete(franquiciaSel);
-				loadFranquicias();
-				cleanForm();
+    }
 
-			} else {
+    public void deleteFranquicia() {
+        try {
+            if (this.franquiciaSel != null) {
+                franquiciaService.delete(franquiciaSel);
+                loadFranquicias();
+                cleanForm();
 
-			}
-		} catch (Exception e) {
+            } else {
 
-		}
-	}
+            }
+        } catch (Exception e) {
 
-	public void selecFranquicia(SelectEvent e) {
-		this.franquiciaSel = (Franquicia) e.getObject();
-	}
+        }
+    }
 
-	public void cleanForm() {
-		this.franquicia = new Franquicia();
-		this.franquiciaSel = null;
-	}
+    public void selecFranquicia(SelectEvent e) {
+        this.franquiciaSel = (Franquicia) e.getObject();
+    }
 
-	public Franquicia getFranquicia() {
-		return franquicia;
-	}
+    public void cleanForm() {
+        this.franquicia = new Franquicia();
+        this.franquiciaSel = null;
+    }
 
-	public void setFranquicia(Franquicia franquicia) {
-		this.franquicia = franquicia;
-	}
+    public Franquicia getFranquicia() {
+        return franquicia;
+    }
 
-	public Franquicia getFranquiciaSel() {
-		return franquiciaSel;
-	}
+    public void setFranquicia(Franquicia franquicia) {
+        this.franquicia = franquicia;
+    }
 
-	public void setFranquiciaSel(Franquicia franquiciaSel) {
-		this.franquiciaSel = franquiciaSel;
-	}
+    public Franquicia getFranquiciaSel() {
+        return franquiciaSel;
+    }
 
-	public List<Franquicia> getFranquicias() {
-		return franquicias;
-	}
+    public void setFranquiciaSel(Franquicia franquiciaSel) {
+        this.franquiciaSel = franquiciaSel;
+    }
 
-	public void setFranquicias(List<Franquicia> franquicias) {
-		this.franquicias = franquicias;
-	}
+    public List<Franquicia> getFranquicias() {
+        return franquicias;
+    }
 
-    
+    public void setFranquicias(List<Franquicia> franquicias) {
+        this.franquicias = franquicias;
+    }
+
 }
