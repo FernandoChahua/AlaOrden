cff361e7ca2ffa37be9ea9a9e7461c74c3e15c72
Diego B <u201616874@upc.edu.pe>
2019-05-05 03:07:21 -0500

    Revert "delete duplicate UsuarioController.java"
    This reverts commit 0bea9aba94135a84166dd9e84eabbcb4af60cc70.

---
+++ web/src/main/java/com/myorg/web/controller/UsuarioController.java
@@ -0,0 +1,101 @@
+package com.myorg.web.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.myorg.core.entity.Usuario;
+import com.myorg.core.service.impl.UsuarioServiceImpl;
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import org.myorg.util.Message;
+import org.primefaces.event.SelectEvent;
+
+@Named
+@ViewScoped
+public class UsuarioController implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Inject
+    private UsuarioServiceImpl usuarioService;
+
+    private List<Usuario> usuarios;
+    private Usuario usuario;
+    private Usuario usuarioSel;
+
+    @PostConstruct
+    public void init() {
+        usuarios = new ArrayList<>();
+        usuario = new Usuario();
+        usuarioSel = new Usuario();
+        this.loadUsuarios();
+    }
+
+    public void loadUsuarios() {
+        try {
+            usuarios = usuarioService.findAll();
+        } catch (Exception e) {
+            Message.messageError("Error Cliente" + e.getMessage());
+        }
+    }
+
+    public void saveUsuario() {
+        try {
+            if (usuario.getIdUsuario() != null) {
+                usuarioService.insert(usuario);
+                Message.messageInfo("Registro actualizado exitosamente");
+            } else {
+                usuarioService.update(usuario);
+                Message.messageInfo("Registro guardado exitosamente");
+            }
+        } catch (Exception e) {
+            Message.messageError("Error Usuario" + e.getMessage());
+        }
+        loadUsuarios();
+        cleanForm();
+    }
+
+//    public void editUsuario() {
+//        try {
+//            System.out.println("Customer Selec:" + usuarioSel.getApodo());
+//            if (this.usuarioSel.getIdUsuario() != null) {
+//                this.usuario = usuarioSel;
+//            } else {
+//                Message.messageInfo("Debe seleccionar un cliente");
+//                System.out.println("Hola:");
+//            }
+//        } catch (Exception e) {
+//            Message.messageError("Error Usuario :" + e.getMessage());
+//        }
+//
+//    }
+
+    public void cleanForm() {
+        this.usuario = new Usuario();
+        this.usuarioSel = null;
+    }
+
+    public void selectUsuario(SelectEvent e) {
+        this.usuarioSel = (Usuario) e.getObject();
+    }
+
+    public List<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(List<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+}
