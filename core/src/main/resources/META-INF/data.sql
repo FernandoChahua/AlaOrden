6ad2d7ce28c13984d5576347a96344299fda54bc
Diego B <u201616874@upc.edu.pe>
2019-05-07 01:23:42 -0500

    correcion a insercion de datos (script) y su condificiacion (UTF-8)

--- core/src/main/resources/META-INF/data.sql
+++ core/src/main/resources/META-INF/data.sql
@@ -1,51 +1,42 @@
-insert into marca
-(nombre)
-values
-('Gloria'),/*1*/
-('Nestle'),/*2*/
-('Negrita'),/*3*/
-('Universal'),/*4*/
-('Field'),/*5*/
-('BLancaFlor'),/*6*/
-('Donofrio'),/*7*/
-('Bimbo'),/*8*/
-('Tottus'),/*9*/
-('Metro'),
-('Wong'),
-('Vivanda'),
-('Ideal');
+insert into marca (nombre) values ('Gloria');
+insert into marca (nombre) values ('Nestle');
+insert into marca (nombre) values ('Negrita');/*3*/
+insert into marca (nombre) values ('Universal');/*4*/
+insert into marca (nombre) values ('Field');/*5*/
+insert into marca (nombre) values ('BLancaFlor');/*6*/
+insert into marca (nombre) values ('Donofrio');/*7*/
+insert into marca (nombre) values ('Bimbo');/*8*/
+insert into marca (nombre) values ('Tottus');/*9*/
+insert into marca (nombre) values ('Metro');
+insert into marca (nombre) values ('Wong');
+insert into marca (nombre) values ('Vivanda');
+insert into marca (nombre) values ('Ideal');
 
 
-insert into categoria
-(nombre) 
-values
-('Lacteos'),/*1*/
-('Bebidas'),/*2*/
-('Carnes'),/*3*/
-('Golosinas'),/*4*/
-('ComidaInstantanea'),/*5*/
-('Frutas'),/*6*/
-('Verduras'),/*7*/
-('Conservas'),/*8*/
-('Detergente'),/*9*/
-('Legumbres'),/*10*/
-('Snacks'),/*11*/
-('Higiene'),/*12*/
-('Belleza'),/*13*/
-('CuidadoPersonal'),/*14*/
-('Licores'),/*15*/
-('Panetones');
+insert into categoria (nombre) values ('Lacteos');/*1*/
+insert into categoria (nombre) values ('Bebidas');/*2*/
+insert into categoria (nombre) values ('Carnes');/*3*/
+insert into categoria (nombre) values ('Golosinas');/*4*/
+insert into categoria (nombre) values ('ComidaInstantanea');/*5*/
+insert into categoria (nombre) values ('Frutas');/*6*/
+insert into categoria (nombre) values ('Verduras');/*7*/
+insert into categoria (nombre) values ('Conservas');/*8*/
+insert into categoria (nombre) values ('Detergente');/*9*/
+insert into categoria (nombre) values ('Legumbres');/*10*/
+insert into categoria (nombre) values ('Snacks');/*11*/
+insert into categoria (nombre) values ('Higiene');/*12*/
+insert into categoria (nombre) values ('Belleza');/*13*/
+insert into categoria (nombre) values ('CuidadoPersonal');/*14*/
+insert into categoria (nombre) values ('Licores');/*15*/
+insert into categoria (nombre) values ('Panetones');
 
 
-insert into producto
-(descripcion,nombre,peso,idCategoria,idMarca) 
-values
-('Leche evaporada sin lactosa','Leche evaporada deslactosada3',300,1,1),
-('Leche evaproada sin lactosa1','Leche evaporada deslactosada',300,1,13),
-('Paneton','Paneton Blanca Flor',300,16,6),
-('Paneton','Paneton Donofrio',300,16,7),
-('Galleta soda bañada en chocolate','ChokoSoda',300,4,5),
-('Galleta de vainilla con cubierta de chocolate','Morocha',300,4,2),
-('Leche condensada','Leche condensada Nestlé',300,1,2),
-('Leche condensada1','Leche condensada Gloria',300,1,1),
-('Paneton','Paneton Bimbo',300,16,8);
+insert into producto (descripcion,nombre,peso,idCategoria,idMarca) values ('Leche evaporada sin lactosa','Leche evaporada deslactosada3',300,1,1);
+insert into producto (descripcion,nombre,peso,idCategoria,idMarca) values ('Leche evaproada sin lactosa','Leche evaporada deslactosada',300,1,13);
+insert into producto (descripcion,nombre,peso,idCategoria,idMarca) values ('Paneton','Paneton Blanca Flor',300,16,6);
+insert into producto (descripcion,nombre,peso,idCategoria,idMarca) values ('Paneton','Paneton Donofrio',300,16,7);
+insert into producto (descripcion,nombre,peso,idCategoria,idMarca) values ('Galleta soda bañada en chocolate','ChokoSoda',300,4,5);
+insert into producto (descripcion,nombre,peso,idCategoria,idMarca) values ('Galleta de vainilla con cubierta de chocolate','Morocha',300,4,2);
+insert into producto (descripcion,nombre,peso,idCategoria,idMarca) values ('Leche condensada','Leche condensada Nestlé',300,1,2);
+insert into producto (descripcion,nombre,peso,idCategoria,idMarca) values ('Leche condensada1','Leche condensada Gloria',300,1,1);
+insert into producto (descripcion,nombre,peso,idCategoria,idMarca) values ('Paneton','Paneton Bimbo',300,16,8);
