0af47cd933445e055aee384d4ca1e527a1d25a25
Fernando <fernandochahua1@gmail.com>
2019-06-10 15:45:51 -0500

    Adios popo

--- src/main/java/com/alaorden/service/DireccionService.java
+++
@@ -1,6 +0,0 @@
-package com.alaorden.service;
-
-import com.alaorden.model.Direccion;
-
-public interface DireccionService extends ServiceCRUD<Direccion>{
-}
--- src/main/java/com/alaorden/service/UsuarioService.java
+++
@@ -1,6 +0,0 @@
-package com.alaorden.service;
-
-import com.alaorden.model.Usuario;
-
-public interface UsuarioService extends ServiceCRUD<Usuario> {
-}
