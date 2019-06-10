ef6064b053e6bad4ceac0603157df89d0cb75c4d
thealexiasilva <adsg999@gmail.com>
2019-06-10 16:23:37 -0500

    franquicia model was modified

--- src/main/java/com/alaorden/model/Franquicia.java
+++ src/main/java/com/alaorden/model/Franquicia.java
@@ -8,18 +8,33 @@ import lombok.NoArgsConstructor;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+import javax.persistence.*;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+
+@Entity
+@Table(name = "Franquicia")
 public class Franquicia {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idFranquicia;
+
+    @Column(name = "nombre")
     private String nombre;
+
+    @Column(name = "url")
     private String url;
+
+
+    @Column(name = "Conexion")
     private String Conexion;
+
+    @Column(name = "Logo")
     private String Logo;
+
+
+    @OneToMany(mappedBy = "franquicia")
     private List<Sede> sedes;
-    private List<ProductoFranquicia> productoFranquicia;
+
 }
