53e65281fc9582d4f8f82f776e999d14014fd2b7
Fernando <fernandochahua1@gmail.com>
2019-06-10 17:44:42 -0500

    Se inserto services direccion

--- src/main/java/com/alaorden/service/DireccionService.java
+++ src/main/java/com/alaorden/service/DireccionService.java
@@ -1,4 +1,14 @@
 package com.alaorden.service;
 
+import com.alaorden.model.Direccion;
+import com.alaorden.model.Usuario;
+
+import java.util.List;
+
 public interface DireccionService {
+
+    Direccion createDireccion(Direccion direccion);
+    Direccion deleteDireccion(int id);
+    List<Direccion> listDireccionByUsuario(int id);
+
 }
--- src/main/java/com/alaorden/service/impl/DireccionServiceImpl.java
+++ src/main/java/com/alaorden/service/impl/DireccionServiceImpl.java
@@ -1,4 +1,44 @@
 package com.alaorden.service.impl;
 
-public class DireccionServiceImpl {
+import com.alaorden.model.Direccion;
+import com.alaorden.model.Usuario;
+import com.alaorden.repository.DireccionRepository;
+import com.alaorden.repository.UsuarioRepository;
+import com.alaorden.service.DireccionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class DireccionServiceImpl implements DireccionService {
+
+    DireccionRepository direccionRepository;
+    UsuarioRepository usuarioRepository;
+    @Autowired
+    public DireccionServiceImpl(DireccionRepository direccionRepository){
+        this.direccionRepository = direccionRepository;
+    }
+
+    public Direccion createDireccion(Direccion direccion){
+        return direccionRepository.save(direccion);
+    }
+
+    public Direccion deleteDireccion(int id){
+        Direccion direccion = direccionRepository.findById(id).get();
+        if(direccion == null){
+            return null;
+        }else{
+            direccionRepository.delete(direccion);
+            return direccion;
+        }
+    }
+    public List<Direccion> listDireccionByUsuario(int id){
+        Usuario usuario = usuarioRepository.findById(id).get();
+        return direccionRepository.findAllByUsuario(usuario);
+    }
+
+    
+
 }
