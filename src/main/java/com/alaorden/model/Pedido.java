1a0c0e713ae0767a5b0c84e541ce7f746d0a91d9
DiegoB <diego.leo994@gmail.com>
2019-06-10 23:01:12 -0500

    Corregido el mapeado... gracias por intentarlo

--- src/main/java/com/alaorden/model/Pedido.java
+++ src/main/java/com/alaorden/model/Pedido.java
@@ -1,11 +1,19 @@
 package com.alaorden.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 @Entity
 @Table(name = "Pedido")
