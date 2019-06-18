c40d1c2853bdc3ac95eb4cc8174e388a7a5a82ab
thealexiasilva <adsg999@gmail.com>
2019-06-17 20:32:20 -0500

    subcategorias route was created

---
+++ frontend/src/routes/SubCategoriasPage/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+import SubCategorias from "../../components/SubCategorias";
+
+
+const SubCategoriasPage = () => (
+    <Layout>
+        <div><SubCategorias/></div>
+    </Layout>
+);
+
+export default SubCategoriasPage;
