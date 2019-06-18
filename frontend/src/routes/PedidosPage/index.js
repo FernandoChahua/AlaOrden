d5db0de9b2540a016a4045609e38b248110f4d43
thealexiasilva <adsg999@gmail.com>
2019-06-17 20:40:24 -0500

    pedidos route was created

---
+++ frontend/src/routes/PedidosPage/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+import Ordenes from "../../components/Ordenes";
+
+
+const OrdenesPage = () => (
+    <Layout>
+        <div><Ordenes/></div>
+    </Layout>
+);
+
+export default OrdenesPage;
