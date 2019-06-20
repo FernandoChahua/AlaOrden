9af75c09ca6e52a923888edea36a51df17adcdeb
Fernando <fernandochahua1@gmail.com>
2019-06-19 23:27:17 -0500

    Incorporacion api google maps to react

--- frontend/src/components/buy/Location.jsx
+++ frontend/src/components/buy/Location.jsx
@@ -6,9 +6,119 @@ import {Button, Dropdown, Form} from "react-bootstrap";
 import Nav from "./PlaceOrder";
 // import direccion from './direccion'
 class Location extends Component {
+
+
+
+    initMap = () =>{
+var marker;
+var map;
+var geocoder;
+var infowindow;
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            geocoder = new window.google.maps.Geocoder;
+            infowindow = new window.google.maps.InfoWindow;
+            map = new window.google.maps.Map(document.getElementById("map"), {
+                center: {
+                    lat: pos.lat,
+                    lng: pos.lng
+                },
+                zoom: 20
+            });
+
+            marker = new window.google.maps.Marker({
+                position: pos,
+                map: map,
+                draggable: false,
+                animation: window.google.maps.Animation.DROP
+            });
+            geocodeLatLng(pos);
+
+            function addMarker(location, map) {
+                marker.setMap(null);
+                marker = new window.google.maps.Marker({
+                    position: location,
+                    map: map,
+                    draggable: false,
+                    animation: window.google.maps.Animation.DROP
+                });
+                map.setCenter(location);
+                map.setZoom(18);
+            }
+            var autocomplete = document.getElementById('autocomplete');
+            const search = new window.google.maps.places.Autocomplete(autocomplete);
+            search.bindTo("bounds", map);
+            search.addListener('place_changed', function () {
+                var place = search.getPlace();
+                if (!place.geometry.viewport) {
+                    window.alert("Error al mostrar el lugar");
+                    return;
+                }
+                if (place.geometry.viewport) {
+                    map.fitBounds(place.geometry.viewport);
+                    geocodeLatLng(place.geometry.location, map);
+                } else {
+                    geocodeLatLng(place.geometry.location, map);
+                }
+
+            });
+
+            function geocodeLatLng(longitud) {
+                var latlng = longitud;
+                geocoder.geocode({
+                    'location': latlng
+                }, function (results, status) {
+                    if (status === 'OK') {
+                        if (results[0]) {
+                            addMarker(longitud, map);
+                            infowindow.setContent(results[0].formatted_address);
+                            infowindow.open(map, marker);
+                            if (document.getElementById("autocomplete"))
+                                document.getElementById("autocomplete").value = results[0].formatted_address;
+                        } else {
+                            window.alert('No results found');
+                        }
+                    } else {
+                        window.alert('Geocoder failed due to: ' + status);
+                    }
+                });
+            }
+            window.google.maps.event.addListener(map, 'click', function (event) {
+                geocodeLatLng(event.latLng);
+            });
+        }, function () {
+            handleLocationError(true, infowindow, map.getCenter());
+        });
+    } else {
+        handleLocationError(false, infowindow, map.getCenter());
+    }
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+}
+    }
+    renderMap = ()=>{
+        loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyDzB-76_WJJt-fAqyqnT23jyCpNwm3jqcg&libraries=places&callback=initMap");
+        window.initMap = this.initMap;
+    }
+    componentDidMount(){
+        this.renderMap();
+
+
+    }
+
+
     render() {
         return (
-            <Container id="map">
+            <Container >
                 <Row>
                     <Col className="md-8">
                         <div>
@@ -89,12 +199,12 @@ class Location extends Component {
 
                         </Form>
 
-                        <section id="map" className="container mt-3 mb-3">
-                        </section>
                     </Col>
 
 
                 </Row>
+                        <section id="map" className="container mt-3 mb-3">
+                        </section>
                 <Dropdown.Divider/>
                 <div>
 
@@ -113,5 +223,19 @@ class Location extends Component {
     }
 
 }
+
+
+function loadScript(url){
+    var script = window.document.createElement("script");
+    var index = window.document.getElementsByTagName("script")[0];
+    script.src = url;
+    script.async = true;
+    script.defer = true;
+    index.parentNode.insertBefore(script,index);
+}
+/*
+<script>
+</script>
+*/
 
 export default Location;
