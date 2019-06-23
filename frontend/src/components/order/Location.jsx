3c008eeccbf521a4793641beb668080f50b090a4
thealexiasilva <adsg999@gmail.com>
2019-06-23 15:57:51 -0500

    Location component was updated

--- frontend/src/components/order/Location.jsx
+++ frontend/src/components/order/Location.jsx
@@ -4,6 +4,7 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import {Button, Dropdown, Form} from "react-bootstrap";
 import Nav from "./PlaceOrder";
+import ProFormaList from "./Quotation/Quotation";
 // import direccion from './direccion'
 class Location extends Component {
 
@@ -118,107 +119,66 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 
     render() {
         return (
-            <Container >
+            <Container className="text-left">
                 <Row>
                     <Col className="md-8">
+                        <br></br>
                         <div>
-                            <h4>Información de Entrega</h4>
+                            <h4>Direccion de Entrega</h4>
                         </div>
+
                         <hr className="mb-2"/>
-                        <Form>
+
+                        <div className="pr-5 m-4">
+
+
+
+
+                            <div className="input-group-append">
+                                <h6 className="mb-3">Direccion Reciente</h6>
+                            </div>
+                            <br></br>
+                            <Form>
+
+                                <select className="form-control" id="selDireccion">
+                                    <option>Calle xxx Nro ###</option>
+                                    <option>Calle yyy Nro ###</option>
+                                </select>
+                            </Form>
+                            <br></br>
+
+                            <div className="d-flex justify-content-between mb-4">
+                                <h6 className="mb-3">Direccion Nueva</h6>
+                                <div className="form-check">
+                                    <input className="form-check-input" type="checkbox" id="chkRecordar"/>
+                                    <Form.Label className="form-check-label" for="chkRecordar">Recordar</Form.Label>
+                                </div>
+                            </div>
+                            <Form>
+                                <section className="container mt-3">
+                                    <input id="autocomplete" className="form-control " type="text"></input>
+                                </section>
+
+                            </Form>
+                            <br></br>
+                            <section id="map" className="container mt-3 mb-3">
+                            </section>
+                            <br></br>
+                            <hr className="mb-2"></hr>
+                            <br></br>
                             <Row>
-                                <Col className="md-6">
-                                    <div className="input-group-append">
-                                        <h6 className="mb-3">Usar Direccion Reciente</h6>
-                                    </div>
-                                    <Form>
-                                        <Form.Label for="selDireccion">Direccion</Form.Label>
-                                        <select className="form-control" id="selDireccion">
-                                            <option>Calle xxx Nro ###</option>
-                                            <option>Calle yyy Nro ###</option>
-                                        </select>
-                                    </Form>
-
-                                    <br></br>
-                                    <div className="row">
-                                        <div className="col-md-6">
-                                            <label htmlFor="txtTitular">Calle</label>
-                                            <input type="text" className="form-control" id="txtCalle"
-                                                   placeholder="Calle"/>
-                                        </div>
-                                        <div className="col-md-4">
-                                            <label htmlFor="txtNumero">Numero</label>
-                                            <input type="text" className="form-control" id="txtNumero" placeholder="Numero"/>
-                                        </div>
-                                    </div>
-
-                                    <br></br>
-                                    <div className="row">
-                                        <div className="col-md-6">
-                                            <label htmlFor="txtTitular">Urbanizacion</label>
-                                            <input type="text" className="form-control" id="txtUrbanizacion"
-                                                   placeholder="Urbanizacion"/>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <label htmlFor="txtCuenta">Distrito</label>
-                                            <input type="text" className="form-control" id="txtDistrito" placeholder="Distrito"/>
-                                        </div>
-                                    </div>
-
-                                    <br></br>
-                                    <div className="row">
-                                        <div className="col-md-12">
-                                            <label htmlFor="txtReferencia">Referencia</label>
-                                            <input type="text" className="form-control" id="txtReferencia"
-                                                   placeholder="Referencia"/>
-                                        </div>
-
-                                    </div>
-
-
+                                <Col>
+                                    <Button variant="outline-primary" block onClick={this.register}>Cancelar</Button>
+                                </Col>
+                                <Col>
+                                    <Button block onClick={this.register}>Siguiente</Button>
                                 </Col>
                             </Row>
-                        </Form>
-
-
-                    </Col>
-                    <Col className="md-4">
-                        <br></br>
-                        <hr className="mb-2"/>
-                        <div className="d-flex justify-content-between mb-4">
-                            <h6>Punto de Entrega</h6>
-                            <div className="form-check">
-                                <input className="form-check-input" type="checkbox" id="chkRecordar"/>
-                                <Form.Label className="form-check-label" for="chkRecordar">Recordar</Form.Label>
-                            </div>
                         </div>
-                        <Form>
-                            <section className="container mt-3">
-                                <input id="autocomplete" className="form-control " type="text"></input>
-                            </section>
-
-                        </Form>
-
                     </Col>
-
-
                 </Row>
-                        <section id="map" className="container mt-3 mb-3">
-                        </section>
-                <Dropdown.Divider/>
-                <div>
-
-                    <div className="mx-1">
-                        <Button type="submit" className="btn btn-secondary">Cancelar</Button>
-                    </div>
-                    <br></br>
-                    <div className="mx-1">
-                        <Button type="submit" className="btn btn-secondary">Siguiente</Button>
-                    </div>
-
-
-                    </div>
             </Container>
+
         )
     }
 
